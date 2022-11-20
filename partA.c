d3df62e25d5807a395eb4006097a7302e0cb1978
kavikushi0228 <kavikushi0207@gmail.com>
2022-11-20 15:42:08 +0530

    check new

---
+++ partA.c
@@ -0,0 +1,6 @@
+#include <fcntl.h> //Permissions
+#include <unistd.h> //Low level I/O
+#include <errno.h> //Stores error number
+#include <stdio.h> //Standard I/O
+#include <stdlib.h> //High level I/O, exit
+//checking new commits
